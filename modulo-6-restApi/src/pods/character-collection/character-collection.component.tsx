8504cef43bce8c64ae571946c7198690bb2442c4
T036875 <mariana.viana.contractor@bbva.com>
2024-10-09 10:44:22 +0200

    feat(restApi): add pagination

--- modulo-6-restApi/src/pods/character-collection/character-collection.component.tsx
+++ modulo-6-restApi/src/pods/character-collection/character-collection.component.tsx
@@ -3,6 +3,9 @@ import Button from '@mui/material/Button';
 import { CharacterEntityVm } from './character-collection.vm';
 import { CharacterCard } from './components/character-card.component';
 import * as classes from './character-collection.styles';
+import { Pagination, PaginationItem } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 interface Props {
   characterCollection: CharacterEntityVm[];
@@ -14,6 +17,19 @@ export const CharacterCollectionComponent: React.FunctionComponent<Props> = (
   props
 ) => {
   const { characterCollection, detail, onEdit } = props;
+  const [page, setPage] = React.useState(1);
+  const itemsPerPage = 6;
+
+  const pageCount = Math.ceil(characterCollection.length / itemsPerPage);
+
+  const paginatedCharacters = characterCollection.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
+
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   return (
     <div className={classes.root}>
@@ -22,12 +38,22 @@ export const CharacterCollectionComponent: React.FunctionComponent<Props> = (
       </Button>
 
       <ul className={classes.list}>
-        {characterCollection.map((character) => (
+        {paginatedCharacters.map((character) => (
           <li key={character.id}>
             <CharacterCard character={character} detail={detail} onEdit={onEdit}/>
           </li>
         ))}
       </ul>
+      <Pagination
+        count={pageCount}
+        onChange={handlePageChange}
+        renderItem={(character) => (
+          <PaginationItem
+            slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+            {...character}
+          />
+        )}
+      />
     </div>
   );
 };
