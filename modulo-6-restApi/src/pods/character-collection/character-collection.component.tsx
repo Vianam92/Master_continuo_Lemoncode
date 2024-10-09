f52f6c09a7be4742bcd227ad84ec2085c2ce1fce
T036875 <mariana.viana.contractor@bbva.com>
2024-10-09 11:04:22 +0200

    feat(restApi): add search

--- modulo-6-restApi/src/pods/character-collection/character-collection.component.tsx
+++ modulo-6-restApi/src/pods/character-collection/character-collection.component.tsx
@@ -3,44 +3,77 @@ import Button from '@mui/material/Button';
 import { CharacterEntityVm } from './character-collection.vm';
 import { CharacterCard } from './components/character-card.component';
 import * as classes from './character-collection.styles';
-import { Pagination, PaginationItem } from '@mui/material';
+import { Input, Pagination, PaginationItem } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 interface Props {
   characterCollection: CharacterEntityVm[];
   detail: (id: string) => void;
-  onEdit: (id: string, character: CharacterEntityVm, bestSentences: string[]) => void;
+  onEdit: (
+    id: string,
+    character: CharacterEntityVm,
+    bestSentences: string[]
+  ) => void;
 }
 
 export const CharacterCollectionComponent: React.FunctionComponent<Props> = (
   props
 ) => {
   const { characterCollection, detail, onEdit } = props;
+  const [searchValue, setSearchValue] = React.useState('');
   const [page, setPage] = React.useState(1);
   const itemsPerPage = 6;
 
   const pageCount = Math.ceil(characterCollection.length / itemsPerPage);
 
-  const paginatedCharacters = characterCollection.slice(
+  const filterActors = () => {
+    if (searchValue === '') {
+      return characterCollection;
+    } else {
+      return characterCollection.filter((character) =>
+        character.name.toLowerCase().includes(searchValue)
+      );
+    }
+  };
+
+  const paginatedCharacters = filterActors().slice(
     (page - 1) * itemsPerPage,
     page * itemsPerPage
   );
 
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
     setPage(value);
+  };
+
+  const handleSearchValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchValue(value.toLowerCase());
   };
 
   return (
     <div className={classes.root}>
-      <Button variant="contained" color="primary">
+      {/*  <Button variant="contained" color="primary">
         Add character
-      </Button>
+      </Button> */}
+      <Input
+        type="text"
+        name="search"
+        placeholder="Busca por actor "
+        onChange={handleSearchValue}
+      />
 
       <ul className={classes.list}>
         {paginatedCharacters.map((character) => (
           <li key={character.id}>
-            <CharacterCard character={character} detail={detail} onEdit={onEdit}/>
+            <CharacterCard
+              character={character}
+              detail={detail}
+              onEdit={onEdit}
+            />
           </li>
         ))}
       </ul>
